cbe1a8ee0815c94ca77d2b76cad887032ce35d02
Baladharun <2022cs0182@svce.ac.in>
2024-11-04 18:30:37 +0530

    login and signup with user from db

--- server/test.cpp
+++ server/test.cpp
@@ -3,11 +3,10 @@
 using namespace std;class Solution {
   public:
     int singleNumber(vector<int>& nums) {
-       int result = 0;
-       for(int i=0;i<nums.size();i++){
-        result^=nums[i];
-       }
-       return result;
+        int result = 0;
+        for(int num:nums)
+            result^=num;
+        return result;
     }
 };int main() {
     vector<int> nums;
